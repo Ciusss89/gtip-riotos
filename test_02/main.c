eb673826f2ef3bd28da165ad014d22c1fc968442
Giuseppe Tipaldi <ing.giuseppe.tipaldi@gmail.com>
2022-10-31 22:36:54 +0100

    test2: fix hardwiring eth controller project
    1. Make it work on current riot-os
    2. Do not use custom ifconfig
    3. Refresh the readme

--- test_02/main.c
+++ test_02/main.c
@@ -11,25 +11,7 @@
 #include "net/ipv4/addr.h"
 #include "shell.h"
 
-static int ifconfig(int argc, char **argv)
-{
-	(void)argc;
-	(void)argv;
-	for (struct netif *iface = netif_list; iface != NULL;
-	     iface = iface->next) {
-
-		printf("%s_%02u: ", iface->name, iface->num);
-		char addrstr[IPV4_ADDR_MAX_STR_LEN];
-
-		printf(" inet %s\n", ipv4_addr_to_str(addrstr,
-				     (ipv4_addr_t *)&iface->ip_addr, sizeof(addrstr)));
-		puts("");
-	}
-	return 0;
-}
-
 static const shell_command_t shell_commands[] = {
-    { "ifconfig", "Shows assigned IPv4/6 addresses", ifconfig },
     { NULL, NULL, NULL }
 };
 
